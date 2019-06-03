f41c234ece5fb92276e33e4890c4782e88d49296
Joseph Ngugi Muiruri <ngugijoseph992@gmail.com>
2019-06-03 18:05:38 +0200

    Update creating_a_table.sql

--- pages/SQL/creating_a_table.sql
+++ pages/SQL/creating_a_table.sql
@@ -10,3 +10,16 @@ INSERT INTO groceries VALUES (1, "Bananas", 4);
 INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
 INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2);
 SELECT * FROM groceries;
+
+
+---============================================================================================================================
+CREATE TABLE library (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+INSERT INTO library VALUES (1,"Kepler Tech",5);
+INSERT INTO library VALUES (2,"Tech",4);
+INSERT INTO library VALUES (3,"API's",2);
+INSERT INTO library VALUES (4,"Azure",1);
+INSERT INTO library VALUES (5,"Linux OS",7);
+INSERT INTO library VALUES (6,"Git introductory",10);
+SELECT * FROM library;
+SELECT * FROM library;
+---==============================================================================================================================
